082b5fc964fc2b6eb1d0dd01a1c1ca0c03be2e0d
rafi-zimraan <raffizimraan27@gmail.com>
2023-02-18 11:21:03 +0700

    first commit

---
+++ soal 2.js
@@ -0,0 +1,12 @@
+// INTRODUCE()
+function introduce(introduce) {
+    console.log(`nama saya ${name} umur saya ${age} lahir di kota ${age} dengan hobby ${hobby} `);
+    
+}
+let name = "Rafi Zimraan";
+let age = 18;
+let address = "yogyakarta";
+let hobby = "running";
+
+let hasil = introduce();
+console.log(hasil);
